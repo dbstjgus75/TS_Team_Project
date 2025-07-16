dbb171665793d94eb0426febda6c9bc28d1d165c
Gino <wltn39@gmail.com>
2025-07-16 17:47:33 +0900

    JS_basic_first

---
+++ Assets/1_JS/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int mStaedId = 1;
+    public GameObject mMyPc;
+    public Transform mNpcSpawnParent;// Parent for NPCs
+    public Transform mSkillObjectParant;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
